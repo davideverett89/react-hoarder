0080ebb5fcc9de60a8aa4ede9ebc0c7e8909184a
David Everett <davideverett1989@gmail.com>
2020-06-06 13:51:09 -0500

    Displaying stuff cards on the page.

--- src/components/shared/StuffCard/StuffCard.js
+++ src/components/shared/StuffCard/StuffCard.js
@@ -6,8 +6,14 @@ class StuffCard extends React.Component {
   render() {
     const { stuff } = this.props;
     return (
-        <div className="StuffCard">
-            <h1>{stuff.itemName}</h1>
+        <div className="StuffCard col-3">
+            <div className="card">
+                <img className="card-img-top" src={stuff.itemImage} alt={stuff.itemName} />
+                <div className="card-body">
+                    <h5 className="card-title">{stuff.itemName}</h5>
+                    <p className="card-text">{stuff.itemDescription}</p>
+                </div>
+            </div>
         </div>
     );
   }
